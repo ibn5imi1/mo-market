b9a9996f9ec803e2198b25f7c8703eaab0961e0e
mohammed <m@gmail.com>
2024-03-23 01:20:19 +0300

    upload mo market project

---
+++ src/components/Products.js
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
+import { NavLink } from "react-router-dom";
+
+const Products = () => {
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState(data);
+  const [loading, setLoading] = useState(false);
+  let componentMount = true;
+  useEffect(() => {
+    const getProducts = async () => {
+      setLoading(true);
+      const response = await fetch("https://fakestoreapi.com/products");
+      if (componentMount) {
+        setData(await response.clone().json());
+        setFilter(await response.json());
+        setLoading(false);
+      }
+      return () => {
+        componentMount = false;
+      };
+    };
+    getProducts();
+  }, []);
+  const Loading = () => {
+    return (
+      <>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+      </>
+    );
+  };
+  const filterProduct = (cat) => {
+    const updateList = data.filter((x) => x.category === cat);
+    setFilter(updateList);
+  };
+  const ShowProducts = () => {
+    return (
+      <>
+        <div className="buttons d-flex justify-content-center mb-5 pb-5">
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => setFilter(data)}
+          >
+            All
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("men's clothing")}
+          >
+            Men's Clothing
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("women's clothing")}
+          >
+            Women's Clothing
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("jewelery")}
+          >
+            Jewelery
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("electronics")}
+          >
+            Electronic
+          </button>
+        </div>
+        {filter.map((Product) => {
+          return (
+            <>
+              <div className="col-lg-3 col-md-6 col-sm-12 mb-4" key={Product.id}>
+                <div className="card h-100 text-center p-4">
+                  <img
+                    src={Product.image}
+                    className="card-img-top"
+                    alt={Product.title}
+                    height="350px"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title mb-0">{Product.title}</h5>
+                    <p className="card-text lead fw-bold">${Product.price}</p>
+                    <NavLink
+                      to={`/products/${Product.id}`}
+                      className="btn btn-outline-dark"
+                    >
+                      Buy Now
+                    </NavLink>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </>
+    );
+  };
+  return (
+    <div>
+      <div className="container my-5 py-5">
+        <div className="row">
+          <div className="col-12 mb-5">
+            <h1 className="display-6 fw-bolder text-center">Latest Products</h1>
+            <hr />
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          {loading ? <Loading /> : <ShowProducts />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
